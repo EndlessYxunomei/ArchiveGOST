1ca26128578d123cffc74fb225a5fdfa60207401
EndlessYxunomei <89537852+EndlessYxunomei@users.noreply.github.com>
2024-01-07 11:20:59 +0300

    создан базовый класс для OriginalDetail

--- VMLayer/CreateOriginalViewModel.cs
+++ VMLayer/CreateOriginalViewModel.cs
@@ -18,96 +18,11 @@ using VMLayer.Validation;
 
 namespace VMLayer;
 
-public class CreateOriginalViewModel: ObservableValidator, INavigationParameterReceiver
+public class CreateOriginalViewModel: OriginalDetailViewModel
 {
-    //Сервисы
-    private readonly INavigationService navigationService;
-    private readonly IDocumentService documentService;
-    private readonly IOriginalService originalService;
-    private readonly ICompanyService companyService;
-    private readonly IPersonService personService;
-    private readonly IDialogService dialogService;
-
-    //Приватные поля
-    private int _inventoryNumber;
-    private string _name = string.Empty;
-    private string _caption = string.Empty;
-    private string? _pageFormat;
-    private string? _notes;
-    private int _pageCount = 1;
-    private CompanyListDto? _company;
-    private DocumentListDto? _document;
-    private PersonListDto? _person;
-
-    //Свойства
-    [Required]
-    [Range(1,int.MaxValue)]
-    public int InventoryNumber
-    {
-        get => _inventoryNumber;
-        set => SetProperty(ref _inventoryNumber, value,true);
-    }
-    [Required]
-    [MaxLength(ArchiveConstants.MAX_ORIGINAL_NAME_LENGTH)]
-    [MinLength(1)]
-    public string Name
-    {
-        get => _name;
-        set => SetProperty(ref _name, value,true);
-    }
-    [Required]
-    [MaxLength(ArchiveConstants.MAX_ORIGINAL_CAPTION_LENGTH)]
-    [MinLength(1)]
-    public string Caption
-    {
-        get => _caption;
-        set => SetProperty(ref _caption, value,true);
-    }
-    [MaxLength(ArchiveConstants.MAX_ORIGINAL_PAGES_FORMAT_LENGTH)]
-    public string? PageFormat
-    {
-        get => _pageFormat;
-        set => SetProperty(ref _pageFormat, value, true);
-    }
-    [Range(1,int.MaxValue)]
-    public int PageCount
-    {
-        get => _pageCount;
-        set => SetProperty(ref _pageCount, value, true);
-    }
-    public CompanyListDto? Company
-    {
-        get => _company;
-        set => SetProperty(ref _company, value);
-    }
-    public DocumentListDto? Document
-    {
-        get => _document;
-        set => SetProperty(ref _document, value);
-    }
-    [MaxLength(ArchiveConstants.MAX_ORIGINAL_NOTES_LENGTH)]
-    public string? Notes
-    {
-        get => _notes;
-        set => SetProperty(ref _notes, value, true);
-    }
-    public PersonListDto? Person
-    {
-        get => _person;
-        set => SetProperty(ref _person, value);
-    }
-
-    //Списки для выбора
-    public ObservableCollection<PersonListDto> PersonList { get; set; } = [];
-    public ObservableCollection<DocumentListDto> DocumentList { get; set; } = [];
-    public ObservableCollection<CompanyListDto> Companylist { get; set; } = [];
-
     //Кнопки
     public IAsyncRelayCommand AcseptCommand { get; }
     public IAsyncRelayCommand CancelCommand { get; }
-    public IAsyncRelayCommand AddDocumentCommand { get; }
-    public IAsyncRelayCommand AddCompanyCommand { get; }
-    public IAsyncRelayCommand AddPersonCommand { get; }
 
     //Методы кнопок
     private async Task CreateOriginal()
@@ -135,7 +50,6 @@ public class CreateOriginalViewModel: ObservableValidator, INavigationParameterR
             //WeakReferenceMessenger.Default.Send(new OriginalUpdatedMessage(newDto));
             //await navigationService.GoBack();
             //Навигация назад с передачей параметра
-            //orginal_list
             await navigationService.GoBackAndReturn(new Dictionary<string, object>() { { NavParamConstants.OrginalList, newDto } });
         }
         else
@@ -146,61 +60,31 @@ public class CreateOriginalViewModel: ObservableValidator, INavigationParameterR
     private async Task CancelCreate()
     {
         await navigationService.GoBack();
-        //return Task.CompletedTask;//ВРОДЕ ТАК
-    }
-    private async Task AddDocument()
-    {
-        await Task.Delay(10);//ЗАглушка
-        DocumentList.Add(new() { DocumentType = AcrhiveModels.DocumentType.AddOriginal, Id = 33, Name = "Test3", Date = DateTime.Today });
-    }
-    private async Task AddCompany()
-    {
-        await Task.Delay(10);//ЗАглушка
-    }
-    private async Task AddPerson()
-    {
-        await Task.Delay(10);//ЗАглушка
     }
 
     //Конструктор
     public CreateOriginalViewModel(INavigationService navigationService, IDocumentService documentService, IOriginalService originalService, IPersonService personService, ICompanyService companyService, IDialogService dialogService)
+        :base(navigationService,documentService, originalService, personService, companyService,dialogService)
     {
-        this.navigationService = navigationService;
-        this.documentService = documentService;
-        this.originalService = originalService;
-        this.personService = personService;
-        this.companyService = companyService;
-        this.dialogService = dialogService;
-
         ErrorExposer = new(this);
         
         AcseptCommand = new AsyncRelayCommand(CreateOriginal, () => !HasErrors);
         CancelCommand = new AsyncRelayCommand(CancelCreate);
 
-        
         //Подписываемся на событие валидации
         ErrorsChanged += CreateOriginalViewModel_ErrorsChanged;
 
-        AddCompanyCommand = new AsyncRelayCommand(AddCompany);
-        AddDocumentCommand = new AsyncRelayCommand(AddDocument);
-        AddPersonCommand = new AsyncRelayCommand(AddPerson);
-
         _ = LoadData();
-        ValidateAllProperties();//пришлось принудительно запускать валидацию, иначе не работало
+        //ValidateAllProperties();//пришлось принудительно запускать валидацию, иначе не работало
     }
     
     //Пероначальная загрузка данных
-    private async Task LoadData()
+    internal override  async Task LoadData()
     {
         var newInventoryNumber = await originalService.GetLustInventoryNumber();
-        var newDocumentList = await documentService.GetDocumentList(AcrhiveModels.DocumentType.AddOriginal);
-        var newCompanyList = await companyService.GetCompanyList();
-        var newPersonList = await personService.GetPersonList();
-
         InventoryNumber = newInventoryNumber + 1;
-        newDocumentList.ForEach(DocumentList.Add);
-        newCompanyList.ForEach(Companylist.Add);
-        newPersonList.ForEach(PersonList.Add);
+
+        await base.LoadData();
     }
 
     //Обработка события валидатора
@@ -208,7 +92,7 @@ public class CreateOriginalViewModel: ObservableValidator, INavigationParameterR
     public ValidationErrorExposer ErrorExposer { get; }
 
     //Обработка навигации
-    public Task OnNavigatedTo(Dictionary<string, object> parameters)
+    public override Task OnNavigatedTo(Dictionary<string, object> parameters)
     {
         if (parameters[NavParamConstants.DocumentList] is DocumentListDto document)
         {
@@ -224,5 +108,4 @@ public class CreateOriginalViewModel: ObservableValidator, INavigationParameterR
         }
         return Task.CompletedTask;
     }
-
 }
--- VMLayer/OriginalDetailViewModel.cs
+++ VMLayer/OriginalDetailViewModel.cs
@@ -1,108 +1,150 @@
-﻿using AcrhiveModels.DTOs;
+﻿using AcrhiveModels;
+using AcrhiveModels.DTOs;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ServiceLayer;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VMLayer.Navigation;
+using VMLayer.Validation;
 
-namespace VMLayer
+namespace VMLayer;
+
+public class OriginalDetailViewModel: ObservableValidator, INavigationParameterReceiver
 {
-    //ПОКА ТАК, ПОТОМ ПРЕДЕЛАТЬ НА БАЗОВЫЙ КЛАСС, ОТ КОТРОГО БУДУТ ПЛЯСАТЬ ВСЕ ОСТАЛЬНЫЕ
-    
-    public class OriginalDetailViewModel: ObservableObject, INavigationParameterReceiver
+    //Сервисы
+    private protected readonly INavigationService navigationService;
+    private protected readonly IDocumentService documentService;
+    private protected readonly IOriginalService originalService;
+    private protected readonly ICompanyService companyService;
+    private protected readonly IPersonService personService;
+    private protected readonly IDialogService dialogService;
+
+    //Приватные поля
+    private protected int _inventoryNumber;
+    private protected string _name = string.Empty;
+    private protected string _caption = string.Empty;
+    private protected string? _pageFormat;
+    private protected string? _notes;
+    private protected int _pageCount = 1;
+    private protected CompanyListDto? _company;
+    private protected DocumentListDto? _document;
+    private protected PersonListDto? _person;
+
+    //Свойства
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int InventoryNumber
     {
-        //Приватные поля
-        private readonly int _id;
-        private int _inventoryNumber;
-        private string _name = string.Empty;
-        private string _caption = string.Empty;
-        private string? _pageFormat;
-        private int _pageCount;
-        private CompanyListDto? _company;
-        private List<CompanyListDto> _companyList = [];
-        private DocumentListDto? _document;
-        private List<DocumentListDto> _documentList = [];
-        private string? _notes;
-        private PersonListDto? _person;
-        private List<PersonListDto> _personList = [];
-
-        //Публичные поля
-        public int InventoryNumber
-        {
-            get => _inventoryNumber;
-            set => SetProperty(ref _inventoryNumber, value);
-        }
-        public string Name
-        {
-            get => _name;
-            set => SetProperty(ref _name, value);
-        }
-        public string Caption
-        {
-            get => _caption;
-            set => SetProperty(ref _caption, value);
-        }
-        public string? PageFormat 
-        { 
-            get => _pageFormat;
-            set => SetProperty(ref _pageFormat,value); 
-        }
-        public int PageCount 
-        { 
-            get => _pageCount; 
-            set => SetProperty(ref _pageCount, value); 
-        }
-        public CompanyListDto? Company
-        {
-            get => _company;
-            set => SetProperty(ref _company, value);
-        }
-        public List<CompanyListDto> Companylist
-        {
-            get => _companyList;
-        }
-        public DocumentListDto? Document
-        {
-            get => _document;
-            set => SetProperty(ref _document, value);
-        }
-        public List<DocumentListDto> DocumentList
-        {
-            get => _documentList;
-        }
-        public string? Notes
-        { 
-            get => _notes; 
-            set => SetProperty(ref _notes, value); 
-        }
-        public PersonListDto? Person
-        {
-            get => _person;
-            set => SetProperty(ref _person, value);
-        }
-        public List<PersonListDto> PersonList
-        {
-            get => _personList;
-        }
-
-        //ещё не реализовано
-        public List<CopyListDto> Copies { get; set; } = [];
-        public List<CorrectionListDto> Corrections { get; set; } = [];
-        public List<ApplicabilityListDto> Applicabilities { get; set; } = [];
-
-        //Кнопки
-
-        //Конструктор
-        public OriginalDetailViewModel()
-        {
-
-        }
-
-        public Task OnNavigatedTo(Dictionary<string, object> parameters)
-        {
-            throw new NotImplementedException();
-        }
+        get => _inventoryNumber;
+        set => SetProperty(ref _inventoryNumber, value, true);
     }
+    [Required]
+    [MaxLength(ArchiveConstants.MAX_ORIGINAL_NAME_LENGTH)]
+    [MinLength(1)]
+    public string Name
+    {
+        get => _name;
+        set => SetProperty(ref _name, value, true);
+    }
+    [Required]
+    [MaxLength(ArchiveConstants.MAX_ORIGINAL_CAPTION_LENGTH)]
+    [MinLength(1)]
+    public string Caption
+    {
+        get => _caption;
+        set => SetProperty(ref _caption, value, true);
+    }
+    [MaxLength(ArchiveConstants.MAX_ORIGINAL_PAGES_FORMAT_LENGTH)]
+    public string? PageFormat
+    {
+        get => _pageFormat;
+        set => SetProperty(ref _pageFormat, value, true);
+    }
+    [Range(1, int.MaxValue)]
+    public int PageCount
+    {
+        get => _pageCount;
+        set => SetProperty(ref _pageCount, value, true);
+    }
+    public CompanyListDto? Company
+    {
+        get => _company;
+        set => SetProperty(ref _company, value);
+    }
+    public DocumentListDto? Document
+    {
+        get => _document;
+        set => SetProperty(ref _document, value);
+    }
+    [MaxLength(ArchiveConstants.MAX_ORIGINAL_NOTES_LENGTH)]
+    public string? Notes
+    {
+        get => _notes;
+        set => SetProperty(ref _notes, value, true);
+    }
+    public PersonListDto? Person
+    {
+        get => _person;
+        set => SetProperty(ref _person, value);
+    }
+
+    //Списки для выбора
+    public ObservableCollection<PersonListDto> PersonList { get; set; } = [];
+    public ObservableCollection<DocumentListDto> DocumentList { get; set; } = [];
+    public ObservableCollection<CompanyListDto> Companylist { get; set; } = [];
+
+    //Кнопки добавления в списки
+    public IAsyncRelayCommand AddDocumentCommand { get; }
+    public IAsyncRelayCommand AddCompanyCommand { get; }
+    public IAsyncRelayCommand AddPersonCommand { get; }
+    private protected async Task AddDocument()
+    {
+        await Task.Delay(10);//ЗАглушка
+        DocumentList.Add(new() { DocumentType = DocumentType.AddOriginal, Id = 33, Name = "Test3", Date = DateTime.Today });
+    }
+    private protected async Task AddCompany()
+    {
+        await Task.Delay(10);//ЗАглушка
+    }
+    private protected async Task AddPerson()
+    {
+        await Task.Delay(10);//ЗАглушка
+    }
+
+    //Конструктор
+    public OriginalDetailViewModel(INavigationService navigationService, IDocumentService documentService, IOriginalService originalService, IPersonService personService, ICompanyService companyService, IDialogService dialogService)
+    {
+        this.navigationService = navigationService;
+        this.documentService = documentService;
+        this.originalService = originalService;
+        this.personService = personService;
+        this.companyService = companyService;
+        this.dialogService = dialogService;
+
+        AddCompanyCommand = new AsyncRelayCommand(AddCompany);
+        AddDocumentCommand = new AsyncRelayCommand(AddDocument);
+        AddPersonCommand = new AsyncRelayCommand(AddPerson);
+
+        ValidateAllProperties();//пришлось принудительно запускать валидацию, иначе не работало
+    }
+
+    //Пероначальная загрузка данных в списки
+    internal virtual async Task LoadData()
+    {
+        var newDocumentList = await documentService.GetDocumentList(DocumentType.AddOriginal);
+        var newCompanyList = await companyService.GetCompanyList();
+        var newPersonList = await personService.GetPersonList();
+
+        newDocumentList.ForEach(DocumentList.Add);
+        newCompanyList.ForEach(Companylist.Add);
+        newPersonList.ForEach(PersonList.Add);
+    }
+    //Заглушка для навигации
+    public virtual Task OnNavigatedTo(Dictionary<string, object> parameters) => Task.CompletedTask;
 }
