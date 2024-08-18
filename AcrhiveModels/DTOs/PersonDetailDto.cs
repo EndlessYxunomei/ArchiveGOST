e6e798a5040bc19279d47c57ac1f1913c5f83c1d
EndlessYxunomei <89537852+EndlessYxunomei@users.noreply.github.com>
2024-08-18 14:50:25 +0300

    создана ДТО для пользователя

--- AcrhiveModels/DTOs/PersonDetailDto.cs
+++ AcrhiveModels/DTOs/PersonDetailDto.cs
@@ -8,5 +8,19 @@ namespace AcrhiveModels.DTOs
 {
 	public class PersonDetailDto
 	{
+		public int Id { get; set; }
+		public string? FirstName { get; set; }
+		public required string LastName { get; set; }
+		public string? Department { get; set; }
+		public static explicit operator PersonDetailDto(Person person)
+		{
+			return new PersonDetailDto()
+			{ 
+				LastName = person.LastName,
+				FirstName = person.FirstName,
+				Department = person.Department,
+				Id = person.Id
+			};
+		}
 	}
 }
